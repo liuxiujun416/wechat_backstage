ef9092443b9e7a2e381f0b43ae74c23d26b6ee5e
liuxiujun416 <15111994842@139.com>
2017-06-16 22:37:02 +0800

    update

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class MovieController extends Controller
