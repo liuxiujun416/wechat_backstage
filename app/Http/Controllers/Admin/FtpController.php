a1a616b02c1f99616504f3bc52999f8932cb63a5
liuxiujun416 <15111994842@139.com>
2017-06-18 00:34:45 +0800

    fix

--- app/Http/Controllers/Admin/FtpController.php
+++ app/Http/Controllers/Admin/FtpController.php
@@ -23,9 +23,9 @@ class FtpController extends Controller
      */
     function __construct($FTP_HOST,$FTP_PORT,$FTP_USER,$FTP_PASS)
     {
-        $this->conn_id = @ftp_connect($FTP_HOST,$FTP_PORT) or die("FTP服务器连接失败");
-        @ftp_login($this->conn_id,$FTP_USER,$FTP_PASS) or die("FTP服务器登陆失败");
-        @ftp_pasv($this->conn_id,1); // 打开被动模拟
+        $this->conn_id = ftp_connect($FTP_HOST,$FTP_PORT) or die("FTP服务器连接失败");
+        ftp_login($this->conn_id,$FTP_USER,$FTP_PASS) or die("FTP服务器登陆失败");
+        ftp_pasv($this->conn_id,0);  // 打开被动模拟
     }
 
     /**
@@ -36,8 +36,8 @@ class FtpController extends Controller
      */
     function up_file($path,$newpath,$type=true)
     {
-      //  if($type) $this->dir_mkdirs($newpath);
-        $this->off = @ftp_put($this->conn_id,$newpath,$path,FTP_BINARY);
+        if($type) $this->dir_mkdirs($newpath);
+        $this->off = ftp_put($this->conn_id,$newpath,$path,FTP_BINARY);
         if(!$this->off) echo "文件上传失败,请检查权限及路径是否正确！";
     }
 
@@ -91,15 +91,15 @@ class FtpController extends Controller
 
         foreach($path_arr as $val)          // 创建目录
         {
-            if(@ftp_chdir($this->conn_id,$val) == FALSE)
-            {
-                $tmp = @ftp_mkdir($this->conn_id,$val);
-                if($tmp == FALSE)
-                {
+            if(ftp_chdir($this->conn_id,$val) == true && $val != '.')
+            {  if(!is_dir($val)) {
+                $tmp = ftp_mkdir($this->conn_id, $val);
+                if ($tmp == FALSE) {
                     echo "目录创建失败,请检查权限及路径是否正确！";
                     exit;
                 }
-                @ftp_chdir($this->conn_id,$val);
+                ftp_chdir($this->conn_id, $val);
+                }
             }
         }
 
