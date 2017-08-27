e50d7894cf5cd97434e75f13c5c0cf3c79dd5a78
liuxiujun416 <472480701@qq.com>
2017-08-28 02:50:59 +0800

    update

--- app/Http/Controllers/Admin/VideoController.php
+++ app/Http/Controllers/Admin/VideoController.php
@@ -9,7 +9,48 @@
 namespace App\Http\Controllers\Admin;
 
 
-class VideoController
+use App\Http\Controllers\Controller;
+use App\Model\Video;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+
+class VideoController extends Controller
 {
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    public function  index()
+    {
+        $lists = [];
+        return view('admin.video.index',['lists'=>$lists]);
+    }
+
+    public function add(Request $request)
+    {
+        if($request->isMethod('post')) {
+            $args = $request->all();
+            $v = Validator::make($args, [
+
+            ]);
+            if($v->fails()) {
+                return back()->withErrors($v->errors());
+            }
+
+            $video = new Video();
+            $video->movie_name = $args['movie_name'];
+            $video->movie_path = $args['movie_path'];
+            $video->status      = $args['status'];
+            $video->img         = $args['img'];
+            $video->created = time();
+            if($video->save($args)) {
+                return redirect('/admin/video/index');
+            } else {
+                return back()->withErrors('添加失败');
+            }
+        }
+        return view('admin.video.add');
+    }
 
 }
